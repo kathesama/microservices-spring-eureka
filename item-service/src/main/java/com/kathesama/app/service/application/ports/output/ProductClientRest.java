038938220d192e64a34c13af6d53c5fe2141af82
kathemica <kathemica@gmail.com>
2024-04-16 21:17:24 -0300

    fixed feing client bad config

--- item-service/src/main/java/com/kathesama/app/service/application/ports/output/ProductClientRest.java
+++ item-service/src/main/java/com/kathesama/app/service/application/ports/output/ProductClientRest.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "product-service", url="http://localhost:8000")
+@FeignClient(name = "product-service")
 public interface ProductClientRest {
     @GetMapping("/products/api/v1")
     public List<ProductResponse> findAll();
