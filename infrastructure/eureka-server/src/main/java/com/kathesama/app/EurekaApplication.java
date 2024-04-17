e71789d272447d60869c2e425d58618a324545ac
kathemica <kathemica@gmail.com>
2024-04-17 15:53:17 -0300

    solving config issues

--- infrastructure/eureka-server/src/main/java/com/kathesama/app/EurekaApplication.java
+++ infrastructure/eureka-server/src/main/java/com/kathesama/app/EurekaApplication.java
@@ -2,10 +2,12 @@ package com.kathesama.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+@RefreshScope
 public class EurekaApplication {
 
 	public static void main(String[] args) {
